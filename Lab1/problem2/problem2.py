d67882909654c62c9481800f7afde7529fcaf9fd
Clara Escorihuela <clara.escorihuela@gmail.com>
2021-11-24 14:33:26 +0100

    third commit it is not working

--- Lab1/problem2/problem2.py
+++ Lab1/problem2/problem2.py
@@ -18,12 +18,12 @@ import numpy as np
 import gym
 import torch
 import matplotlib.pyplot as plt
-
+import pickle
 # Import and initialize Mountain Car Environment
 env = gym.make('MountainCar-v0')
 env.reset()
 k = env.action_space.n      # tells you the number of actions
-low, high = env.observation_space.low, env.observation_space.high
+low, high = env.observation_space.low, env.observation_space.high #tells us the low, high limits for the state (state == pos,vel)
 
 # Parameters
 N_episodes = 100        # Number of episodes to run for training
@@ -32,6 +32,25 @@ discount_factor = 1.    # Value of gamma
 
 # Reward
 episode_reward_list = []  # Used to save episodes reward
+
+#----------------------------------------------------------------------------------------------------------------------
+# CREATE FOURIER BASIS
+#-----------------------------------------------------------------------------------------------------------------------
+
+def basis_function(eta, state):
+
+    "Function that will transform the states to the new basis"
+    basis = np.zeros((np.shape(eta)[0]))
+    for i in range (np.shape(eta)[0]):
+        base_i = np.cos(np.pi*np.dot(np.transpose(eta[:,i]),state))
+        basis[i] = base_i
+    return basis.transpose()
+
+def Q_calculation(w, basis, action):
+    Q_new = np.dot(np.transpose(w[:,action]),basis)
+    return Q_new
+
+
 
 
 # Functions used during training
@@ -51,19 +70,34 @@ def scale_state_variables(s, low=env.observation_space.low, high=env.observation
     x = (s - low) / (high - low)
     return x
 
+def choose_action(epsilon, w, basis):
+    if np.random.uniform(0, 1) < epsilon:
+        action = np.random.choice(np.arange(k))
+    else:
+        action = np.argmax([Q_calculation(w,basis,action) for action in range(k)])
+    return action
 
 # Training process
 for i in range(N_episodes):
     # Reset enviroment data
     done = False
     state = scale_state_variables(env.reset())
+    eta = np.array([[0,0],[1,0],[1,0],[1,1]]).transpose()
     total_episode_reward = 0.
+    epsilon = 0.01
+    w = np.zeros((np.shape(eta)[0],k))
+    z = np.zeros((np.shape(eta)[0],k))
+    gamma = 1
+    lamda = 0.9
+    alpha = 0.01
 
     while not done:
-        # Take a random action
+        # Take an action
         # env.action_space.n tells you the number of actions
         # available
-        action = np.random.randint(0, k)
+        env.render()
+        basis = basis_function(eta, state)
+        action = choose_action(epsilon, w, basis)
             
         # Get next state and reward.  The done variable
         # will be True if you reached the goal position,
@@ -71,6 +105,16 @@ for i in range(N_episodes):
         next_state, reward, done, _ = env.step(action)
         next_state = scale_state_variables(next_state)
 
+        # Choose the next action
+        next_basis = basis_function(eta, next_state)
+        next_action = choose_action(epsilon, w, next_basis)
+
+        #Update z
+        z[:,action] = gamma*lamda* z[:,action]+basis
+
+        #Update w
+        delta = reward + gamma*Q_calculation(w, next_basis, next_action)-Q_calculation(w,basis,action)
+        w = w + alpha*delta*z
         # Update episode reward
         total_episode_reward += reward
             
@@ -82,7 +126,9 @@ for i in range(N_episodes):
 
     # Close environment
     env.close()
-    
+
+with open('weights.pkl', 'wb') as f:
+    pickle.dump(w,f)
 
 # Plot Rewards
 plt.plot([i for i in range(1, N_episodes+1)], episode_reward_list, label='Episode reward')
